8cef0f4641d696a37c68c48b044ca52a08e97cc5
cc@macos.air <cc@macos.air>
2018-11-29 10:01:46 +0100

    Use aria2.sh directly

--- aria2.sh
+++ aria2.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-aria2c --enable-rpc --rpc-listen-all=true --rpc-allow-origin-all --rpc-secret=aria2
+aria2c --enable-rpc --rpc-listen-all=true --rpc-allow-origin-all --rpc-secret=aria2 -d ~/
