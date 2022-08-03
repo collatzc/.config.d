f0ceed46e0859f092ce3e17f02fa30a098794f24
cc <cc@t480.wsl>
2022-08-03 21:07:23 +0200

    !win:useFontNormal

--- installOhMyZsh.sh
+++ installOhMyZsh.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sh -c "$(curl -fsSL https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh)"
