9aa209256bccbf18a8c5e837b812cf28089633d9
CC <cc@collation.mg>
2022-10-04 19:01:10 +0200

    +installFonts

--- installZsh.sh
+++ installZsh.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+mkdir ~/.local/share/fonts
+ln -s ~/.config.d/fonts/linux ~/.local/share/fonts/
+
 sudo pacman -S zsh git curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
