12543bd0bf7e3fe3b485e7681966f294d0e933e8
Your Name <Your Name>
2024-08-11 23:38:27 +0200

    +install Rime>Linux

---
+++ Rime/Linux/install.sh
@@ -0,0 +1,7 @@
+ln -s ~/.config.d/Rime/Linux/default.custom.yaml ~/.config/ibus/rime/
+ln -s ~/.config.d/Rime/Linux/double_pinyin_flypy.schema.yaml ~/.config/ibus/rime/
+ln -s ~/.config.d/Rime/schema/essay-zh-hans.txt ~/.config/ibus/rime/
+ln -s ~/.config.d/Rime/schema/extended.dict.yaml ~/.config/ibus/rime/
+ln -s ~/.config.d/Rime/schema/pinyin_simp.dict.yaml ~/.config/ibus/rime/
+ln -s ~/.config.d/Rime/schema/placeless_punct.dict.yaml ~/.config/ibus/rime/
+ln -s ~/.config.d/Rime/schema/zh-hans-t-essay-bgw.gram
