1825b73a6493f9e2eeae8e56948e02a1765771ac
cc <cc@Cs-MacBook-Pro.local>
2025-05-11 20:14:52 +0200

    ! LazyVim: mason-workaround

--- installOhMyZsh.sh
+++ installOhMyZsh.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 #sh -c "$(curl -fsSL https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
